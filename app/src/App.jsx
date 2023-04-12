740581f201131f16042729b948fe24fa353c2476
YungLawn <cteelon@usca.edu>
2023-04-12 16:22:37 -0400

    init

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import HumanIdle from './assets/spritesheets/human/idle/IsoHumanIdle-E.png'
+// import HumanWalk from './assets/spritesheets/human/walk'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='char'></div>
+    </div>
+  )
+}
+
+export default App
