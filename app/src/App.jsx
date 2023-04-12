1e45fd9c04d7f2d8f9d1c2f956817b4747d6b526
YungLawn <cteelon@usca.edu>
2023-04-12 17:56:58 -0400

    its alive!!

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,11 +4,48 @@ import HumanIdle from './assets/spritesheets/human/idle/IsoHumanIdle-E.png'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [dir,setDir] = useState('s')
+
+  function handleKeyPress(event) {
+
+    const { key } = event;
+
+   if (key.keyCode === 87) {
+      console.log('here')
+
+      setDir('nw');
+
+    } else if (key === 's' && 'd') {
+
+      setDir('sw');
+
+    } else if (key === 's' && 'd') {
+
+      setDir('se');
+
+    } else if (key === 'w') {
+
+      setDir('n');
+
+    } else if (key === 'a') {
+
+      setDir('w');
+
+    } else if (key === 's') {
+
+      setDir('s');
+
+    } else if (key === 'd') {
+
+      setDir('e');
+
+    }
+
+  }
 
   return (
-    <div className="App">
-      <div className='char'></div>
+    <div className="App" onKeyDown={(event) => {handleKeyPress(event)}}>
+      <div className='char' style={{animationName: 'walk-'+ dir}}></div>
     </div>
   )
 }
